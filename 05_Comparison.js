450cd613139beea0604cdbc0499a3891fd4b648c
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-04 18:57:54 +0000

    Test

---
+++ 05_Comparison.js
@@ -0,0 +1,29 @@
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+
+console.log("2" > 1);
+console.log("02">1);
+
+
+console.log(null > 0); //false
+console.log(null < 0);  //false
+console.log(null == 0); //false
+
+
+console.log(undefined < 0); //false
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+
+
+/*
+
+Note:-
+
+1. === operator checks datatype along with value
+ */
---
+++ datatype-summary.js
@@ -0,0 +1,9 @@
+//PRIMITIVE DATATYPES
+
+/*
+
+1. These data types are call by value we are not given their original values we are given a copy of these values and whatever changes we make are made in the copy of that data
+
+2.  
+
+ */
