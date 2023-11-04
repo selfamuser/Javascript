32754a19f5398219e24e37ff3b5a1a333f01cc7d
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-04 08:36:49 +0000

    Test

--- 02_Datatypes.js
+++ 02_Datatypes.js
@@ -1,1 +1,35 @@
-"use strict";  // It treats all JS Code as newer version.
+
+//STRING
+let name = "Test User"
+
+//NUMBER 
+let age = 21
+
+//BOOLEAN
+let register = true;
+
+//Undefined
+let ans;
+
+//Symbol
+
+//Object
+
+
+
+//NOTES -------------
+
+/*
+
+1. For documentation please refer ECMA script.
+
+2. alert() is not used in editors it is mostly used in browers.
+
+3. "use strict" it is used to enable to run javascript on newer versions.
+
+4. Range of NUMBER datatype is 2 to the power 53
+
+5. null it is the representation of EMPTY VAlUe
+
+6. Undefined is when value is not intialized/assigned
+ */
