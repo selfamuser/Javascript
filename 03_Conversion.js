ee7a86e8f0bdb2919bf1eda659acd80e6e49908a
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-04 18:40:37 +0000

    test

--- 03_Conversion.js
+++ 03_Conversion.js
@@ -1,19 +1,27 @@
-let score = undefined;
+// let score = undefined;
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let newScore = Number(score);
-console.log(typeof newScore);
+// let newScore = Number(score);
+// console.log(typeof newScore);
 
-console.log(newScore);
+// console.log(newScore);
+
+
+
+let isLogged = 1;
+
+let booleanIsLogged = Boolean(isLogged);
+
+console.log(booleanIsLogged);
 
 //NOTES-----------
 
 /*
 
-WHEN WE CONVERT VARIABLE TYPE
+WHEN WE CONVERT VARIABLE TYPE to NUMBER
 
 1. When we convert string to number it gives the given number in string
     Ex :- "33" ==> 33
@@ -21,5 +29,13 @@ WHEN WE CONVERT VARIABLE TYPE
     Ex :- "33Abc" ==> NaN
 3. In case of Boolean values true is converted to 1 and false to 0
     Ex :- true ==> 1, false ==> 0
+
+
+
+WHEN WE CONVER TO BOOLEAN
+
+1. 1 Is converted to true 0 to false
+
+2. Empty string is false and text is true
 
 */
---
+++ 04_Operations.js
@@ -0,0 +1,32 @@
+
+
+//Power
+
+console.log(2**3);
+
+//Concatenate Strings
+
+let str1 = "abc";
+let str2 = "def";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+
+console.log(1+3+"44");
+
+console.log("44" + 1 + 3);
+
+
+//NOTES-----------
+
+/* 
+
+1. When during concatination follow the rules
+
+2. If String + Number + Number will act as string
+
+3. If Number + Number + String gives Number sum converted to string
+
+*/
