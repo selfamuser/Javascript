8f5c6f997c172d394578cfa19ad5132e93f11f25
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-04 19:06:18 +0000

    t

--- datatype-summary.js
+++ datatype-summary.js
@@ -4,6 +4,23 @@
 
 1. These data types are call by value we are not given their original values we are given a copy of these values and whatever changes we make are made in the copy of that data
 
-2.  
+2.  String, Number, Boolean, null, undefined, Symbol, BigInt
 
  */
+
+
+//NON PRIMITIVE / Reference Type
+
+/*
+
+1. ARRAY, OBJECTS, FUNCTION
+
+*/
+
+
+
+//------------
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+//
