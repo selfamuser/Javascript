104344b02dff449a67f3b13012f3b4d175afd279
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-07 10:13:46 +0000

    test

---
+++ Section3/01_loops.js
@@ -0,0 +1,57 @@
+
+
+// //For --------
+
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// } 
+
+
+// //break
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if(element == 6){
+//         console.log("My job is done till 6 now want to exit")
+//         break
+//     }
+//     console.log(element);
+// }
+
+
+// //continue
+
+// for (let index = 1; index <= 20; index++) {
+//     const element = index;
+//     if(element == 6){
+//         console.log(`Detected 6`);
+//         continue
+//     }
+//     console.log(element);
+// }
+
+
+//While ---------
+
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(`Value of index is ${i}`);
+//     i++;
+// }
+
+
+//do while--------
+
+//It runs minimum one time before checking the condition
+
+
+let score = 1;
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=5)
