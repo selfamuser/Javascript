89c6b78d39ad97fe2f1337fc5a634a30865963ab
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-07 07:26:39 +0000

    test

--- Section2/04_function.js
+++ Section2/04_function.js
@@ -32,6 +32,7 @@ const user = {
     age : 24,
     welcomeMessage : function(){
         console.log(`Hello ${this.name} to the website`);
+        console.log(this);
     }
 }
 
@@ -40,6 +41,29 @@ user.welcomeMessage();
 user.name = "Sanchit"
 
 user.welcomeMessage();
+
+
+//To create arraow 
+
+const func = () =>{
+    let userName = "Utkarsh"
+    console.log(this);
+}
+func();
+
+
+// Implicit Return ------
+
+const addTwo = (num1,num2) => num1 + num2
+
+// In the above line we didn;t write the return keyword but we still got the answer because if we write the code in one line without return it acts as implicit return function.
+
+
+console.log(addTwo(2,3));
+
+
+
+
 
 
 
