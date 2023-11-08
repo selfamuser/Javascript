2f7c4528763c3629833deb06f54c3912b5190e05
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-08 18:28:05 +0000

    test

---
+++ Section3/04_reduce.js
@@ -0,0 +1,14 @@
+/*
+
+const arr = [1,2,3,4]
+
+const initialValue = 0;
+const sumWithInitial = arr.reduce(
+    (accumulator,currentValue) => accumulator + currentValue, initialValue
+);
+
+console.log(sumWithInitial)
+
+Output = 10;
+
+*/
