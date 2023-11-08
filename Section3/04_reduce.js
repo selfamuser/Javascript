b0616ae468913e5a03b9cd707705b5d421dd4f83
selfamuser <57310485+selfamuser@users.noreply.github.com>
2023-11-08 18:43:19 +0000

    test

--- Section3/04_reduce.js
+++ Section3/04_reduce.js
@@ -1,4 +1,5 @@
 /*
+SYNTAX-----
 
 const arr = [1,2,3,4]
 
@@ -12,3 +13,32 @@ console.log(sumWithInitial)
 Output = 10;
 
 */
+
+
+
+//Example
+
+const shoppingCart = [
+    {
+        itemName : "T-Shirt",
+        price : 1000
+    },
+    {
+        itemName : "Jeans",
+        price : 2000
+    },
+    {
+        itemName : "Overcoat",
+        price : 11999
+    },
+    {
+        itemName : "Belt",
+        price : 500
+    }
+]
+
+const priceToPay = shoppingCart.reduce((finalPrice,item)=>{
+    return finalPrice + item.price
+},0)
+
+console.log(priceToPay);
